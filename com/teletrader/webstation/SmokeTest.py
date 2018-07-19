9af7aa3adc9b89d20a8dbf5c77379fa10666f425
ivanori1 <coric.ivan@yandex.com>
2018-07-19 16:48:15 +0200

    successful login  (#3 test case)

--- com/teletrader/webstation/SmokeTest.py
+++ com/teletrader/webstation/SmokeTest.py
@@ -6,7 +6,7 @@ class SmokeTest(unittest.TestCase):
     # Instance of Chrome browser
     driver = webdriver.Chrome()
 
-    # Elements
+    # CheckBoxes
 
     def test(self):
         # Opens test branch
@@ -20,7 +20,13 @@ class SmokeTest(unittest.TestCase):
         # Assert eula error
         self.assertEqual("You have to accept the End User License Agreement in order to log in."
                          , self.driver.find_element_by_class_name("error_container_inner").text)
-
+        # Successful login
+        self.driver.find_element_by_id("userName").clear()
+        self.driver.find_element_by_id("userName").send_keys("ivan.coric91")
+        self.driver.find_element_by_id("password").send_keys("ictrader123")
+        self.driver.find_element_by_css_selector("[for='eulaAccepted'].checkBoxLabel").click()
+        self.driver.find_element_by_id("loginUser").click()
+        # TODO Assertation for logo-ws.is_displayed
 
 run = SmokeTest()
 run.test()
