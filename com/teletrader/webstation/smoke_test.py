39e17c310ba5280ac7708d837f1bbc68d295d45e
ivanori1 <coric.ivan@yandex.com>
2018-07-24 14:04:11 +0200

    import logging

--- com/teletrader/webstation/smoke_test.py
+++ com/teletrader/webstation/smoke_test.py
@@ -1,6 +1,7 @@
 import unittest
 from com.teletrader.webstation.login_test import TestLogin
 from com.teletrader.webstation.navigation_button_test import TestNavigationButtons
+import logging
 
 # Get all test cases
 tc_login = unittest.TestLoader().loadTestsFromTestCase(TestLogin)
@@ -9,3 +10,4 @@ tc_nav_b = unittest.TestLoader().loadTestsFromTestCase(TestNavigationButtons)
 # Create test
 smoke_test = unittest.TestSuite([tc_login, tc_nav_b])
 unittest.TextTestRunner().run(smoke_test)
+logging.basicConfig(filename='test.log', level=logging.DEBUG)
