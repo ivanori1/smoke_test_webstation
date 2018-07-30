62052b7c16696a4e1b0966d2c974f71a09014f90
ivanori1 <coric.ivan@yandex.com>
2018-07-30 14:14:57 +0200

    get_by_type (By class), get_element with find_element(By class, locator) and click_on_element as get_element().click and text_of_element get_element().text

---
+++ webstation/base/selenium_driver.py
@@ -0,0 +1,36 @@
+from selenium.webdriver.common.by import By
+
+
+class SeleniumDriver:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def get_by_type(self, locator_type):
+        if locator_type == "css":
+            return By.CSS_SELECTOR
+        else:
+            print("Unknown selector")
+
+    def get_element(self, locator, locator_type="css"):
+        element = None
+        try:
+            by_type = self.get_by_type(locator_type)
+            element = self.driver.find_element(by_type, locator)
+        except:
+            print("element not found")
+        return element
+
+    def click_on_element(self, locator, locator_type="css"):
+        try:
+            element = self.get_element(locator, locator_type)
+            element.click()
+        except:
+            print("click action not performed")
+
+    def text_of_element(self, inner_text, locator, locator_type="css"):
+        element_text = self.get_element(locator, locator_type).text
+        if element_text == inner_text:
+            return True
+        else:
+            print("Compared text do not match")
+            return False
