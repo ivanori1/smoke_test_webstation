af4543f871282a25e1e666e731d6c27a0d64f8ea
ivanori1 <coric.ivan@yandex.com>
2018-08-01 15:26:06 +0200

    test logout add, also name of test change to have order, still pytest not implement

--- webstation/tests/test_login.py
+++ webstation/tests/test_login.py
@@ -2,17 +2,26 @@ import unittest
 import pytest
 from selenium import webdriver
 from webstation.pages.login_page import LoginPage
+from webstation.pages.header_page import HeaderPage
 
 
 class TestLogin(unittest.TestCase):
     driver = webdriver.Chrome()
     login_p = LoginPage(driver)
+    header_p = HeaderPage(driver)
 
-    def test_failed_login(self):
+    @pytest.mark.run(order=1)
+    def test_1_failed_login(self):
         self.login_p.open_start_page()
         self.login_p.test_failed_login()
         self.assertTrue(self.login_p.verify_failed_login())
 
-    def test_successful_login(self):
+    @pytest.mark.run(order=2)
+    def test_2_successful_login(self):
         self.login_p.test_success_login()
-        self.assertTrue(self.login_p.verify_success_login())
+        self.assertTrue(self.header_p.verify_success_login())
+
+    @pytest.mark.run(order=3)
+    def test_3_logout(self):
+        self.header_p.perform_logout()
+        #self.assertTrue(self.login_p.verify_auto_login_and_eula_checked())
