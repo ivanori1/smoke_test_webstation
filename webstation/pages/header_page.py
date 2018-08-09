131da39c0989f9d321a5576da03e336cb727fa45
ivanori1 <coric.ivan@yandex.com>
2018-08-09 12:04:23 +0200

    layout buttons, actions, and perform_change_layout_vertical add finally if statement :is_navigation_visible

--- webstation/pages/header_page.py
+++ webstation/pages/header_page.py
@@ -8,9 +8,13 @@ class HeaderPage(SeleniumDriver):
         self.driver = driver
 
     # Elements
+    _navigation_vertical_container = ".navigation-vertical-container"
     _logo_ws_image = "#logo-ws"
     _user_button = ".header_button#user-button"
     _logout_link = "[href='Logout.aspx']"
+    _layout_button = ".layout-button"
+    _full_vertical_navigation_button = "[data-layout-id='full-vertical-navigation']"
+    _save_layout_button = "#saveLayout"
 
     def is_visible_logo(self):
         self.is_visible_element(self._logo_ws_image)
@@ -22,6 +26,15 @@ class HeaderPage(SeleniumDriver):
     def click_logout_link(self):
         self.click_on_element(self._logout_link)
 
+    def click_layout_button(self):
+        self.click_on_element(self._layout_button)
+
+    def click_full_vertical_navigation(self):
+        self.click_on_element(self._full_vertical_navigation_button)
+
+    def click_save_layout_button(self):
+        self.click_on_element(self._save_layout_button)
+
     def verify_success_login(self):
         result = HeaderPage.is_visible_logo
         return result
@@ -29,3 +42,13 @@ class HeaderPage(SeleniumDriver):
     def perform_logout(self):
         self.click_user_button()
         self.click_logout_link()
+
+    def perform_change_layout_vertical(self):
+        self.click_layout_button()
+        self.click_full_vertical_navigation()
+        self.click_save_layout_button()
+
+    def is_navigation_visible(self):
+        result = self.is_visible_element(self._navigation_vertical_container)
+        if not result:
+            self.perform_change_layout_vertical()
