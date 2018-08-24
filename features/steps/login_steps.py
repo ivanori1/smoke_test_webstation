937b4f789ae2840967bf31153a7d9390c433abaa
ivanori1 <coric.ivan@yandex.com>
2018-08-24 16:05:43 +0200

    Select language add

--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -1,4 +1,4 @@
-from behave import step
+from behave import step, then
 
 
 @step('Open WebStation login page')
@@ -6,11 +6,32 @@ def step_impl(context):
     context.selenium_driver.navigate("http://webstationtest3.ttweb.net/WebStation.aspx")
 
 
+@step('Select language "{language}"')
+def step_impl(context, language):
+    context.login_page.select_language(language)
+
+
 @step('Click on Login button')
 def step_impl(context):
     context.login_page.click_login_button()
 
 
-@step('Error appears: "{error}"')
+@then('Error appears: "{error}"')
 def step_impl(context, error):
-    context.login_page.verify_failed_login(error)
+    assert context.login_page.verify_failed_login(error)
+
+
+@step('Username "{username}" and password "{password}"')
+def step_impl(context, username, password):
+    context.login_page.type_username_placeholder(username)
+    context.login_page.type_password_placeholder(password)
+
+
+@step('Stay logged in option is "{status}"')
+def step_impl(context, status):
+    context.login_page.check_autologin(status)
+
+
+@step('Accept EULA is "{status}"')
+def step_impl(context, status):
+    context.login_page.check_eula(status)
