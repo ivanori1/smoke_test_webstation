d0ec1bd8124418046bece8a49dcc5134a5cb054b
ivanori1 <coric.ivan@yandex.com>
2018-08-24 16:06:08 +0200

    select_language and changes for check methods

--- features/pages/login_page.py
+++ features/pages/login_page.py
@@ -11,11 +11,18 @@ class LoginPage(SeleniumDriver):
     _autologin_checkbox_xpath = "//*[@id='autologin']"
     _login_button = "#loginUser"
     _error_box = ".error_container_inner"
-    # Errors
-    _accept_eula_error = "You have to accept the End User License Agreement in order to log in."
+    _language_box = "#langButton1"
+    _language_box_en = "#langNavien"
+    _language_box_de = "#langNavide"
+
+    def select_language(self, language):
+        self.click_on_element(self._language_box)
+        if language == "English":
+            self.click_on_element(self._language_box_en)
+        elif language == "Deutch":
+            self.click_on_element(self._language_box_de)
 
     def type_username_placeholder(self, username):
-        print(self.attribute_value_of_element("placeholder", self._username_placeholder))
         self.send_keys_to_element(username, self._username_placeholder)
 
     def type_password_placeholder(self, password):
@@ -25,16 +32,25 @@ class LoginPage(SeleniumDriver):
         eula_TF = self.is_selected_element(self._eula_checkbox_xpath)
         return eula_TF
 
-    def check_eula(self):
-        if not self.is_eula_checked():
-            self.click_on_element(self._eula_checkbox)
+    def check_eula(self, status):
+        if status == "checked":
+            if not self.is_eula_checked():
+                self.click_on_element(self._eula_checkbox)
+        elif status == "unchecked":
+            if self.is_eula_checked():
+                self.click_on_element(self._eula_checkbox)
 
     def is_autologin_checked(self):
         autologinTF = self.is_selected_element(self._autologin_checkbox_xpath)
         return autologinTF
-    def check_autologin(self):
-        if not self.is_autologin_checked():
-            self.click_on_element(self._autologin_checkbox)
+
+    def check_autologin(self, status):
+        if status == "checked":
+            if not self.is_autologin_checked():
+                self.click_on_element(self._autologin_checkbox)
+        elif status == "unchecked":
+            if self.is_autologin_checked():
+                self.click_on_element(self._autologin_checkbox)
 
     def click_login_button(self):
         self.click_on_element(self._login_button)
@@ -46,7 +62,7 @@ class LoginPage(SeleniumDriver):
     def test_failed_login(self):
         self.click_login_button()
 
-    def verify_failed_login(self,error):
+    def verify_failed_login(self, error):
         result = self.text_of_element(error, self._error_box)
         return result
 
@@ -59,4 +75,3 @@ class LoginPage(SeleniumDriver):
     def verify_auto_login_and_eula_checked(self):
         result = self.is_autologin_checked() and self.is_eula_checked()
         return result
-
