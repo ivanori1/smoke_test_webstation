74a0a6c0dcc3d9327c5b120c89a68e6e8eac19a7
ivanori1 <coric.ivan@yandex.com>
2018-08-29 17:44:02 +0200

    assert for @step('Navigation Box is visible')

--- features/steps/navigation_steps.py
+++ features/steps/navigation_steps.py
@@ -3,4 +3,14 @@ from behave import step, then
 
 @step('Navigation Box is visible')
 def step_impl(context):
-    context.header_page.navigation_box_is_visible()
+    assert context.header_page.navigation_box_is_visible()
+
+
+@step('Click on News Button')
+def step_impl(context):
+    context.header_page.click_news_navigation()
+
+
+@step('Click on customize and control button')
+def step_impl(context):
+    context.header_page.click_on_costomize_and_control()
