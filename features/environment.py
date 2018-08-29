bf8daa34787b8e5860d7df0487ddb134ce655e8c
ivanori1 <coric.ivan@yandex.com>
2018-08-29 15:42:11 +0200

    context.header_page = HeaderPage() return: before_all

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,13 @@
 from features.base.selenium_driver import SeleniumDriver
 from features.pages.login_page import LoginPage
+from features.pages.header_page import HeaderPage
 
 
 def before_all(context):
     context.selenium_driver = SeleniumDriver()
     context.login_page = LoginPage()
+    context.header_page = HeaderPage()
+
+
+def after_all(context):
+    context.selenium_driver.close()
