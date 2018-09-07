4c2e1a3d439e4168217104bea329a4bc2b507222
ivanori1 <coric.ivan@yandex.com>
2018-09-07 16:44:42 +0200

    Step Logout inside Home if statement

--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -61,3 +61,6 @@ def step_impl(context, page):
         assert context.login_page.verify_login_button_visible()
     if page == "Home":
         assert context.login_page.verify_home_button_visible()
+        context.execute_steps(u"""
+        then Logout
+        """)
